7c7712d2f1f36f288463865787b032e040d3037f
bleistivt <bleistivt@users.noreply.github.com>
2015-06-26 09:38:46 +0200

    casening

--- class.unsubscribediscussion.plugin.php
+++ class.unsubscribediscussion.plugin.php
@@ -1,192 +1,167 @@
-<?php if (!defined('APPLICATION')) exit();
+<?php
 
-$PluginInfo['UnsubscribeDiscussion'] = array(
+$PluginInfo['UnsubscribeDiscussion'] = [
     'Name' => 'Unsubscribe Discussion',
     'Description' => 'Adds the ability to selectively turn off notifications for individual discussions.',
     'Version' => '1.0.1',
-    'RequiredApplications' => array('Vanilla' => '2.1'),
+    'RequiredApplications' => ['Vanilla' => '2.1'],
     'HasLocale' => true,
     'MobileFriendly' => true,
     'Author' => 'Bleistivt',
     'AuthorUrl' => 'http://bleistivt.net',
     'License' => 'GNU GPL2'
-);
+];
 
 class UnsubscribeDiscussionPlugin extends Gdn_Plugin {
 
     //Ajax endpoint for the unsubscribe click
-    public function DiscussionController_Unsubscribe_Create($Sender, $Args) {
-        $Session = Gdn::Session();
-        if (!$Session->UserID) {
-            throw PermissionException('SignedIn');
-        }
-        if (!$Sender->Request->IsAuthenticatedPostBack()
-            && !$Session->ValidateTransientKey(val(1, $Args))
-        ) {
-            throw PermissionException();
+    public function discussionController_unsubscribe_create($sender, $discussionID) {
+        if (!Gdn::request()->isAuthenticatedPostBack()) {
+            throw permissionException();
         }
 
-        $DiscussionModel = new DiscussionModel();
-        $Discussion = $DiscussionModel->GetID(val(0, $Args));
-        if (!$Discussion) {
-            throw NotFoundException('Discussion');
+        $model = new DiscussionModel();
+        if (!$discussion = $model->getID(val(0, $discussionID))) {
+            throw notFoundException('Discussion');
         }
 
         // WHERE array for convenience
-        $Wheres = array(
-            'DiscussionID' => $Discussion->DiscussionID,
-            'UserID' => $Session->UserID
-        );
+        $where = [
+            'DiscussionID' => $discussion->DiscussionID,
+            'UserID' => Gdn::session()->UserID
+        ];
 
         // Get the UserDiscussion data.
-        $UserDiscussion = $DiscussionModel->SQL
-            ->GetWhere('UserDiscussion', $Wheres)
-            ->FirstRow(DATASET_TYPE_ARRAY);
+        $userDiscussion = $model->SQL->getWhere('UserDiscussion', $where)->firstRow();
 
-        if ($UserDiscussion) {
-            $Unsubscribed = !$UserDiscussion['Unsubscribed'];
+        if ($userDiscussion) {
             // Update the existing row.
-            $DiscussionModel->SQL
-                ->Put('UserDiscussion', array('Unsubscribed' => (int)$Unsubscribed), $Wheres);
+            $unsubscribed = !$userDiscussion->Unsubscribed;
+            $model->SQL->put('UserDiscussion', ['Unsubscribed' => (int)$unsubscribed], $where);
         } else {
-            $Unsubscribed = true;
-            $Wheres['Unsubscribed'] = 1;
             // Insert a new row.
-            $DiscussionModel->SQL
-                ->Options('Ignore', true)
-                ->Insert('UserDiscussion', $Wheres);
+            $unsubscribed = true;
+            $where['Unsubscribed'] = 1;
+            $model->SQL->options('Ignore', true)->insert('UserDiscussion', $where);
         }
-        $Discussion->Unsubscribed = $Unsubscribed;
+        $discussion->Unsubscribed = $unsubscribed;
 
-        if ($Unsubscribed) {
-            $Sender->InformMessage(
-                Sprite('Eye', 'InformSprite').T('You will no longer be notified about this discussion.'),
+        if ($unsubscribed) {
+            $sender->informMessage(
+                sprite('Eye', 'InformSprite').t('You will no longer be notified about this discussion.'),
                 'Dismissable AutoDismiss HasSprite'
             );
         } else {
-            $Sender->InformMessage(
-                Sprite('Eye', 'InformSprite').T('You will receive new notifications about this discussion.'),
+            $sender->informMessage(
+                sprite('Eye', 'InformSprite').t('You will receive new notifications about this discussion.'),
                 'Dismissable AutoDismiss HasSprite'
             );
-        }
-
-        // Redirect for non-js users
-        if ($Sender->DeliveryType() == DELIVERY_TYPE_ALL) {
-            $Target = GetIncomingValue('Target', '/discussions');
-            SafeRedirect($Target);
         }
 
         // Highlight the discussion item.
-        $Sender->JsonTarget("#Discussion_{$Discussion->DiscussionID}", null, 'Highlight');
-        $Sender->JsonTarget(".Discussion #Item_0", null, 'Highlight');
+        $sender->jsonTarget("#Discussion_{$discussion->DiscussionID}", null, 'Highlight');
+        $sender->jsonTarget(".Discussion #Item_0", null, 'Highlight');
 
         // Update the options flyout.
-        $Sender->SendOptions($Discussion);
+        $sender->sendOptions($discussion);
 
-        $Sender->Render('Blank', 'Utility', 'Dashboard');
+        $sender->render('blank', 'utility', 'dashboard');
     }
 
     // Add the "Unsubscribed" field to the discussion queries.
-    public function DiscussionModel_BeforeGet_Handler($Sender) {
-        if (Gdn::Session()->UserID > 0) {
-            $Sender->SQL->Select('w.Unsubscribed');
+    public function discussionModel_beforeGet_handler($sender) {
+        if (Gdn::session()->isValid()) {
+            $sender->SQL->select('w.Unsubscribed');
         }
     }
 
-    public function DiscussionModel_BeforeGetID_Handler($Sender) {
-        if (Gdn::Session()->UserID > 0) {
-            $Sender->SQL->Select('w.Unsubscribed');
-        }
+    public function discussionModel_beforeGetID_bandler($sender) {
+        $this->discussionModel_beforeGet_bandler($sender);
     }
 
     // Intercept the notification queue to remove unsubscribed discussion notifications.
-    public function CommentModel_BeforeNotification_Handler($Sender) {
+    public function commentModel_beforeNotification_handler($sender) {
         // Find out which discussion we are in.
-        $Discussion = false;
-        foreach (ActivityModel::$Queue as $QItem) {
-            if (isset($QItem['Comment'][0]['RecordID'])) {
-                $Record = GetRecord('comment', $QItem['Comment'][0]['RecordID']);
-                if ($Record) {
-                    $Discussion = $Record['Discussion'];
+        $discussion = false;
+        foreach (ActivityModel::$Queue as $item) {
+            if (isset($item['Comment'][0]['RecordID'])) {
+                $record = getRecord('comment', $item['Comment'][0]['RecordID']);
+                if ($record) {
+                    $discussion = $record['Discussion'];
                     break;
                 }
             }
         }
-        if (!$Discussion) {
+        if (!$discussion) {
             return;
         }
         // Get all UserIDs that have unsubscribed the discussion.
-        $Unsubscribed = Gdn::SQL()
-            ->Select('UserID')
-            ->From('UserDiscussion')
-            ->Where(array(
-                'DiscussionID' => $Discussion['DiscussionID'],
+        $unsubscribed = Gdn::sql()
+            ->select('UserID')
+            ->from('UserDiscussion')
+            ->where([
+                'DiscussionID' => $discussion['DiscussionID'],
                 'Unsubscribed' => 1
-            ))
-            ->Get()
-            ->Result(DATASET_TYPE_ARRAY);
-        $Unsubscribed = ConsolidateArrayValuesByKey($Unsubscribed, 'UserID');
+            ])
+            ->get()
+            ->resultArray();
+        $unsubscribed = array_column($unsubscribed, 'UserID');
 
         // Unset all notification queue items for these users.
-        foreach (ActivityModel::$Queue as $UserID => $QItem) {
-            if (in_array($UserID, $Unsubscribed)) {
-                unset(ActivityModel::$Queue[$UserID]);
+        foreach (ActivityModel::$Queue as $userID => $item) {
+            if (in_array($userID, $unsubscribed)) {
+                unset(ActivityModel::$Queue[$userID]);
             }
         }
     }
 
     // Add the unsubscribe/resubscribe option
-    public function Base_DiscussionOptions_Handler($Sender) {
-		$Session = Gdn::Session();
-		if (!$Session->IsValid()) {
-			return;
-		}
-        $Discussion = $Sender->EventArguments['Discussion'];
+    public function base_discussionOptions_handler($sender, &$args) {
+        if (!Gdn::session()->isValid()) {
+            return;
+        }
+        $discussion = $args['Discussion'];
 
         // Get the Unsubscribed status, if it is not set.
         // This is only needed for announcements, as GetAnnouncements doesn't fire a BeforeGet event.
-        if (!isset($Discussion->Unsubscribed)) {
-            $UserDiscussion = Gdn::SQL()
-                ->Select('Unsubscribed')
-                ->From('UserDiscussion')
-                ->Where(array(
-                    'DiscussionID' => $Discussion->DiscussionID,
-                    'UserID' => Gdn::Session()->UserID
-                ))
-                ->Get()
-                ->FirstRow(DATASET_TYPE_ARRAY);
-            $Discussion->Unsubscribed = (bool)$UserDiscussion['Unsubscribed'];
+        if (!isset($discussion->Unsubscribed)) {
+            $userDiscussion = Gdn::sql()
+                ->select('Unsubscribed')
+                ->from('UserDiscussion')
+                ->where([
+                    'DiscussionID' => $discussion->DiscussionID,
+                    'UserID' => Gdn::session()->UserID
+                ])
+                ->get()
+                ->firstRow();
+            $discussion->Unsubscribed = (bool)$userDiscussion->Unsubscribed;
         }
 
-        $Option = array(
-            'Label' => T($Discussion->Unsubscribed ? 'Resubscribe' : 'Unsubscribe'),
-            'Url' => '/discussion/unsubscribe/'
-                .$Discussion->DiscussionID.'/'
-                .Gdn::Session()->TransientKey()
-                .'?Target='.urlencode($Sender->SelfUrl),
-            'Class' => $Discussion->Unsubscribed ? 'Resubscribe Hijack' : 'Unsubscribe Hijack'
-        );
+        $item = [
+            'Label' => t($discussion->Unsubscribed ? 'Resubscribe' : 'Unsubscribe'),
+            'Url' => '/discussion/unsubscribe/'.$discussion->DiscussionID,
+            'Class' => $discussion->Unsubscribed ? 'Resubscribe Hijack' : 'Unsubscribe Hijack'
+        ];
 
-        if (isset($Sender->EventArguments['DiscussionOptions'])) {
+        if (isset($args['DiscussionOptions'])) {
             // Discussion options in discussion view
-            $Sender->EventArguments['DiscussionOptions']['UnsubscribeDiscussion'] = $Option;
+            $args['DiscussionOptions']['UnsubscribeDiscussion'] = $item;
         } else {
             // Discussion options in discussions view
-            $Sender->Options .= Wrap(Anchor($Option['Label'], $Option['Url'], $Option['Class']), 'li');
+            $sender->Options .= wrap(anchor($item['Label'], $item['Url'], $item['Class']), 'li');
         }
     }
 
     // Add a new "Unsubscribed" column to the UserDiscussion table.
-    public function Structure() {
-        Gdn::Database()
-            ->Structure()
-            ->Table('UserDiscussion')
-            ->Column('Unsubscribed', 'tinyint(1)', '0')
-            ->Set();
+    public function structure() {
+        Gdn::structure()
+            ->table('UserDiscussion')
+            ->column('Unsubscribed', 'tinyint(1)', '0')
+            ->set();
     }
 
-    public function Setup() {
-        $this->Structure();
+    public function setup() {
+        $this->structure();
     }
 
 }
